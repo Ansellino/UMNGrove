cacf4c9a9e4d39c98d94d5039a24a265794be5de
AnselZorro <ansellino@gmail.com>
2025-02-20 19:25:18 +0700

    feat: fix the UI Design

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('Lavioxel Shop - Skin Care', 'Lavioxel Shop - Skin Care') }}</title>
+        <title>{{ config('Lembur Mangrove Patikang', 'Lembur Mangrove Patikang') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
